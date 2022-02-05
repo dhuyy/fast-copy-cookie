505be6da55be200aa67af3b81ee1d3690f74eea3
Dhuy Martins <vandhuy@gmail.com>
2022-02-05 01:04:42 +0100

    Initial commit with an chrome extension example

---
+++ .prettierrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  singleQuote: true, // INFO: non-default option (default: false)
+  trailingComma: 'es5', // INFO: non-default option (default: "none")
+  proseWrap: 'always',
+  arrowParens: 'avoid',
+};
